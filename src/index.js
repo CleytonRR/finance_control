7cafa8d377d83832329eb7dbaeaff42dbb72a382
cleytonRR <cleytonrodrigues69@live.com>
2020-02-13 00:32:00 -0300

    installed and configure helmetJS

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const cors = require('cors')
+const helmet = require('helmet')
 const routes = require('./routes')
 
 const app = express()
+app.use(helmet())
 app.use(cors())
 
 app.use(express.json())
